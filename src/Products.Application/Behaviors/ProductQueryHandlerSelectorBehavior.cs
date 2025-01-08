0f119ab9fbc0a5f9a707cfcead0870d994332030
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-08 19:33:00 +0530

    ProductQueryHandlerSelectorBehavior

---
+++ src/Products.Application/Behaviors/ProductQueryHandlerSelectorBehavior.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Products.Application.DTOs;
+using Products.Application.Products.Queries;
+using Products.Application.Products.Queries.ProductsByDate;
+using Products.Application.Products.Queries.ProductsByPrice;
+
+namespace Products.Application.Behaviors;
+
+public class ProductQueryHandlerSelectorBehavior<TRequest, TResponse> :
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest : GetProductsQuery, IRequest<TResponse>
+    where TResponse : List<ProductDto>
+{
+    private readonly IMediator _mediator;
+
+    public ProductQueryHandlerSelectorBehavior(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        IRequest<List<ProductDto>> queryToSend = default!;
+
+        if (request.MinPrice.HasValue && request.MaxPrice.HasValue)
+        {
+            queryToSend = new ProductsByPriceQuery
+            {
+                MinPrice = request.MinPrice.Value,
+                MaxPrice = request.MaxPrice.Value,
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize
+            };
+        }
+        else if (request.StartDate.HasValue && request.EndDate.HasValue)
+        {
+            queryToSend = new ProductsByDateQuery
+            {
+                StartDate = request.StartDate.Value,
+                EndDate = request.EndDate.Value,
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize
+            };
+        }
+
+        return (TResponse)await _mediator.Send(queryToSend, cancellationToken);
+    }
+}
