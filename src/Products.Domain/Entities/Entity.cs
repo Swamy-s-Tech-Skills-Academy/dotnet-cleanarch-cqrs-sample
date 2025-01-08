203a45f52fe709623cb6b463fd0bcbbbb37ae433
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-08 16:31:53 +0530

    Added Entitites

---
+++ src/Products.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace Products.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+
+    public string CreatedBy { get; set; } = "Admin";
+
+    public DateTime CreatedDate { get; set; }
+
+    public string? ModifiedBy { get; set; } = "Admin";
+
+    public DateTime? ModifiedDate { get; set; }
+}
---
+++ src/Products.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Domain.Entities;
+
+public class Product : Entity
+{
+    public string Name { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+    public Guid CategoryId { get; set; }
+
+    public Category Category { get; set; } = default!;
+}
