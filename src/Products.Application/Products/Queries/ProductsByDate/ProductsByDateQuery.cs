6c3b9d09526354877104f51162a9b57092c2556d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-08 18:56:51 +0530

    Products by Date

---
+++ src/Products.Application/Products/Queries/ProductsByDate/ProductsByDateQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Products.Application.DTOs;
+
+namespace Products.Application.Products.Queries.ProductsByDate;
+
+public class ProductsByDateQuery : IRequest<List<ProductDto>>
+{
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 10;
+}
---
+++ src/Products.Application/Products/Queries/ProductsByDate/ProductsByDateQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Products.Application.DTOs;
+using Products.Domain.Filters;
+using Products.Domain.Interfaces.Repositories;
+
+namespace Products.Application.Products.Queries.ProductsByDate;
+
+public class ProductsByDateQueryHandler(IProductsRepository productRepository, ICategoriesRepository categoryRepository, IMapper mapper) : IRequestHandler<ProductsByDateQuery, List<ProductDto>>
+{
+    private readonly IProductsRepository _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+    private readonly ICategoriesRepository _categoriesRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+    private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+    public async Task<List<ProductDto>> Handle(ProductsByDateQuery request, CancellationToken cancellationToken)
+    {
+        var filter = new ProductFilter
+        {
+            StartDate = request.StartDate,
+            EndDate = request.EndDate,
+            PageNumber = request.PageNumber,
+            PageSize = request.PageSize
+        };
+
+        var products = await _productRepository.GetProductsAsync(filter);
+
+        var productDtos = _mapper.Map<List<ProductDto>>(products);
+
+        foreach (var productDto in productDtos)
+        {
+            var category = await _categoriesRepository.GetCategoryByIdAsync(productDto.CategoryId);
+
+            productDto.CategoryName = category?.Name!;
+        }
+
+        return productDtos;
+    }
+}
