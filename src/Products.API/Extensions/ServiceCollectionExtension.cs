4a928c07272a3376b0559211c66f210ee2efedbf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-10 09:33:00 +0530

    Changes

---
+++ src/Products.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Products.Application.Extensions;
+using Products.Infrastructure.Extensions;
+
+namespace Products.API.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddPresentation(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        services.AddOpenApi();
+
+        services.AddApplication();
+
+        services.AddInfrastructure(configuration);
+
+    }
+}
---
+++ src/Products.API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Infrastructure.Persistence;
+using Products.Infrastructure.Persistence.Seeders;
+
+namespace Products.API.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseHttpPipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+
+            using IServiceScope? scope = app.Services.CreateScope();
+            IServiceProvider? services = scope.ServiceProvider;
+
+            try
+            {
+                StoreDbContext? context = services.GetRequiredService<StoreDbContext>();
+                context.Database.Migrate(); // Ensure database is created/updated
+
+                ICategoriesSeeder? categoriesSeeder = services.GetRequiredService<ICategoriesSeeder>();
+                await categoriesSeeder.SeedAsync();
+
+                IProductsSeeder? productsSeeder = services.GetRequiredService<IProductsSeeder>();
+                await productsSeeder.SeedAsync();
+            }
+            catch (Exception ex)
+            {
+                ILogger<Program>? logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred seeding the Store DB.");
+            }
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
